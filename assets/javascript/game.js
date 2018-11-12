0cbfa36183fdd99855e98b423eaf8e7dea99d9c3
Mayja Tom <maja.morales17@gmail.com>
2018-11-12 12:03:21 -0800

    adding psuedo code to game.js file as outline of attack

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+//player showed random number between 19-120. 
+
+//when player clicks on a crystal, it will add a specific amount of points to players total score
+
+//crystal button value between 1-12. 
+
+//player wins if total score matches the random number shown in the beginning
+
+//player loses if their score goes above random number
+
+//the game restarts whenever the player wins or loses. 
+
+//when game restarts, player is shown new random number. All crystals will have 4 new hidden values. User score reset to zero. 
+
+//show running count of number of games won/lost
+
+
+
